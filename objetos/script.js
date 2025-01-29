bb38c4230fb7900cf71782a5a35d043e034afd40
mateusrtt <mateuseduardo779@gmail.com>
2025-01-28 22:42:43 -0300

    Continuação da aula de objetos

--- objetos/script.js
+++ objetos/script.js
@@ -1,8 +1,21 @@
-function Carro(marca, preco) {
+/*function Carro(marca, preco) {
     this.marca = marca;
     this.preco = preco;
   }
-  
   const honda = new Carro('Honda', 4000);
   const fiat = new Carro('Fiat', 3000);
+  */
+
+  function Dom(seletor) {
+    const element = document.querySelector(seletor);
+    this.ativo = function(classe) {
+      element.classList.add(classe);
+    };
+  }
+  
+  const lista = new Dom('ul');
+  lista.ativo('ativo');
+  
+  const lastLi = new Dom('li:last-child');
+  lastLi.ativo('ativo');
   
