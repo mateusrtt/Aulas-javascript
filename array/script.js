1702a4f8176aad1e1caf354505af67164d10ca9e
mateusrtt <mateuseduardo779@gmail.com>
2025-02-02 22:55:02 -0300

    Resolução de exercicios da aula de array

--- array/script.js
+++ array/script.js
@@ -1,78 +1,49 @@
- // const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-// const precos = [49, 99, 69, 89];
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
 
-// const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+const primeiroValor = comidas.shift();
+const ultimoValor = comidas.pop();
 
-// const carros = new Array('Ford', 'Fiat', 'Honda');
+comidas.push('Arroz');
+comidas.unshift('Peixe', 'Batata')
 
-// carros[2] = 'Ferrari';
-// carros[3] = 'Kia';
-// carros[20] = 'Kia';
+console.log(primeiroValor);
+console.log(comidas);
 
-// console.log(carros.length)
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
 
-// const li = document.querySelectorAll('li');
+estudantes.sort();
+estudantes.reverse();
 
-// const arrayLi = Array.from(li)
+console.log(estudantes);
+console.log(estudantes.includes('Joana'))
+console.log(estudantes.includes('Juliana'))
 
-// const obj = {
-//   0: 'Andre',
-//   1: 'Rafael',
-//   2: 'Teste',
-//   length: 4,
-// }
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
 
-// const objArray = Array.from(obj);
+html = html.split('section').join('ul').split('div').join('li');
+console.log(html);
 
-// console.log(li)
-// console.log(arrayLi)
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
 
-// const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+const carrosCopia = carros.slice();
 
-// console.log(frutas[2][0].length)
+carros.pop();
 
-// const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
-// instrumentos.sort();
-
-// const idades = [32,21,33,43,1,12,8];
-// idades.sort();
-
-// console.log(instrumentos)
-// console.log(idades)
-
-const carros = ['Ford', 'Fiat', 'VW'];
-carros.unshift('Kia', 'Ferrari');
-const novaArray = carros.push('Parati', 'Gol');
-
-// console.log(novaArray)
-
-// console.log(carros);
-
-// console.log(carros.shift())
-
-// console.log(carros);
-// console.log(carros.splice(2,4,'Fusca'))
-
-// console.log(carros);
-
-// console.log(['Item1', 'Item2', 'Item3', 'Item4', 'item5'].copyWithin(2, 0, 2));
-
-// console.log(['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 0, 3));
-
-// const transporte1 = ['Barco', 'Aviao'];
-// const transporte2 = ['Carro', 'Moto'];
-
-
-// let htmlString = '<h2>Título Principal</h2>';
-// htmlString = htmlString.split('h2');
-// htmlString = htmlString.join('h1')
-
-// console.log(htmlString)
-
-const linguagens = ['html', 'css', 'js', 'php', 'python'];
-
-const cloneLinguagens = linguagens.slice();
-
-console.log(linguagens.pop())
-console.log(linguagens)
-console.log(cloneLinguagens)
+console.log(carros);
+console.log(carrosCopia);
