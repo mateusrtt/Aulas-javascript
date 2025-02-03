10e1508bdc747507ad01b5f0c74ddde335bddd2d
mateusrtt <mateuseduardo779@gmail.com>
2025-02-03 00:31:12 -0300

    Finalizado a resolução de exercicios da aula de array e interações

--- array-e-interacao/script.js
+++ array-e-interacao/script.js
@@ -1,23 +1,72 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
-// Remova o primeiro valor de comidas e coloque em uma variável
-// Remova o último valor de comidas e coloque em uma variável
-// Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
-const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// Arrume os estudantes em ordem alfabética
-// Inverta a ordem dos estudantes
-// Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
+const cursos = document.querySelectorAll('.curso');
+const arrayCursos = Array.from(cursos);
 
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`
-// Substitua section por ul e div com li,
-// utilizando split e join
+const objetoCurso = arrayCursos.map((curso) =>{
+    const titulo = curso.querySelector('h1').innerText;
+    const descricao = curso.querySelector('p').innerText;
+    const aula = curso.querySelector('.aulas').innerText;
+    const horas = curso.querySelector('.horas').innerText;
+    return {
+        titulo,
+        descricao,
+        aula,
+        horas,
+    }
+})
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
+console.log(objetoCurso);
+console.log(arrayCursos);
+
+// Retorne uma lista com os
+// números maiores que 100
+
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQue100 = numeros.filter(n => n > 100);
+
+console.log(maioresQue100);
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const possuiBaixo = instrumentos.some((item) => {
+    return item === 'Baixo';
+})
+
+console.log(possuiBaixo);
+
+// Retorne o valor total das compras
+
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+const valorTotal = compras.reduce((acumulador, item) =>{
+    const precoLimpo = +item.preco.replace('R$', '').replace(',','.');
+    return acumulador + precoLimpo;
+},0)
+
+console.log(valorTotal)
