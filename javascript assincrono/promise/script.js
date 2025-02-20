f331a8600ac899b5a5091b508cd46c603bfab643
mateusrtt <mateuseduardo779@gmail.com>
2025-02-20 15:08:54 -0300

    Finalizada aula de promises

--- javascript assincrono/promise/script.js
+++ javascript assincrono/promise/script.js
@@ -1,10 +1,54 @@
-const promessa = new Promise(function(resolve, reject) {
+/*
+const promessa = new Promise((resolve, reject) => {
     let condicao = true;
     if(condicao) {
-      resolve();
+    setTimeout(()=>{
+        resolve({nome:'mateus', idade: 21})
+    }, 1000)
     } else {
-      reject();
+      reject(Error('Um erro ocorreu na promise'));
     }
   });
 
-  console.log(promessa);
+const retorno = promessa
+.then(resolucao => {
+    console.log(resolucao);
+    resolucao.profissao = 'Front-end';
+    return resolucao;
+})
+.then(resolucao =>{
+    console.log(resolucao);
+}, rejeitada =>{
+    console.log(rejeitada);
+}).finally(()=>{
+    console.log('acabou');
+})
+
+console.log(retorno)
+*/
+
+const login = new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Login Efetuado');
+    }, 1000);
+  });
+  const dados = new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Dados Carregados');
+    }, 1500);
+  });
+  
+  const tudoCarregado = Promise.all([login, dados]);
+  
+  tudoCarregado.then(respostas => {
+    console.log(respostas); // Array com ambas respostas
+  });
+  
+
+
+
+
+
+
+
+
