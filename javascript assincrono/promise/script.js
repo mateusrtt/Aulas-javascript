c333ce53556e622986850ff2d2a72816232d0502
mateusrtt <mateuseduardo779@gmail.com>
2025-02-20 14:37:48 -0300

    Inicio da aula de promises

---
+++ javascript assincrono/promise/script.js
@@ -0,0 +1,10 @@
+const promessa = new Promise(function(resolve, reject) {
+    let condicao = true;
+    if(condicao) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+
+  console.log(promessa);
